f126ed8b33a5ee7bf21536313044befc15438716
Tristenh <tristenhobba1@gmail.com>
2023-06-24 20:10:44 +1000

    added dayForecast querySelectorAll, for loop dayForecast, futureDay = data.list[i * 8 ] there's 8 timestamps per day,skips them to get next day, if i >= 0 forcast.innerHTML gets desired properties of futureDay, fixed spelling mistake in HTML

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const form = document.querySelector(".form");
 const input = document.querySelector(".input");
 const cityInfo = document.querySelector(".city-info");
+const dayForecast = document.querySelectorAll(".day-forecast");
 
 let getDate = new Date();
-const currentDay = getDate.toLocaleDateString(1 / 12 / 2023);
+const currentDay = getDate.toLocaleDateString();
 
 var APIKey = "a5a686e64400da39c5b4faa4a396c1b4";
 form.addEventListener("submit", function (event) {
@@ -27,11 +28,25 @@ function getApi(city) {
     .then(function (data) {
       cityInfo.innerHTML = "";
       console.log(data);
-      cityInfo.innerHTML = ` 
-        <h2>${data.city.name} (${currentDay})<img src= "https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png?"/>
-        </h2>
-        <p>Temp: ${data.list[0].main.temp} °C</p>
-        <p>Wind: ${data.list[0].wind.speed} km/h</p>
-        <p>Humidity: ${data.list[0].main.humidity} %</p>`;
+
+      for (let i = 0; i < dayForecast.length; i++) {
+        let forecast = dayForecast[i];
+        let futureDay = data.list[i * 8];
+        if (i >= 0) {
+          cityInfo.innerHTML = ` 
+          <h2>${data.city.name} (${currentDay})<img src= "https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png?"/>
+          </h2>
+          <p>Temp: ${data.list[0].main.temp} °C</p>
+          <p>Wind: ${data.list[0].wind.speed} km/h</p>
+          <p>Humidity: ${data.list[0].main.humidity} %</p>`;
+
+          forecast.innerHTML = `
+          <h2>(${futureDay.dt_txt})<img src= "https://openweathermap.org/img/wn/${futureDay.weather[0].icon}@2x.png?"/>
+          </h2>
+          <p>Temp: ${futureDay.main.temp} °C</p>
+          <p>Wind: ${futureDay.wind.speed} km/h</p>
+          <p>Humidity: ${futureDay.main.humidity} %</p>`;
+        }
+      }
     });
 }
