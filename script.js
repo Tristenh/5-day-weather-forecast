52ce0defd189ad7093f96415614faf6c9f53e813
Tristenh <tristenhobba1@gmail.com>
2023-06-24 18:23:30 +1000

    refactor HTML, refactor CSS, using openweathermap forecast api instead of openweathermap weather, updated cityInfo,innerHTML template literal (data) properties

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const form = document.querySelector(".form");
 const input = document.querySelector(".input");
 const cityInfo = document.querySelector(".city-info");
 
+let getDate = new Date();
+const currentDay = getDate.toLocaleDateString(1 / 12 / 2023);
+
 var APIKey = "a5a686e64400da39c5b4faa4a396c1b4";
 form.addEventListener("submit", function (event) {
   var city = input.value;
@@ -11,7 +14,7 @@ form.addEventListener("submit", function (event) {
 
 function getApi(city) {
   var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
     city +
     "&appid=" +
     APIKey +
@@ -22,21 +25,13 @@ function getApi(city) {
       return response.json();
     })
     .then(function (data) {
-      let getDate = new Date();
-
-      cityInfo.innerHTML = ` 
-      <h2>${data.name} (${getDate.toLocaleDateString(
-        1 / 12 / 2023
-      )})<img src= "https://openweathermap.org/img/wn/${
-        data.weather[0].icon
-      }@2x.png?"/>
-      </h2>
-     <p>Temp: ${data.main.temp}</p>
-     <p>Wind: ${data.wind.speed}km/h</p>
-     <p>Humidity: ${data.main.humidity}%</p>`;
-
-      console.log(data.main.temp);
+      cityInfo.innerHTML = "";
       console.log(data);
+      cityInfo.innerHTML = ` 
+        <h2>${data.city.name} (${currentDay})<img src= "https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png?"/>
+        </h2>
+        <p>Temp: ${data.list[0].main.temp} °C</p>
+        <p>Wind: ${data.list[0].wind.speed} km/h</p>
+        <p>Humidity: ${data.list[0].main.humidity} %</p>`;
     });
 }
-// ${data.weather.icon}
