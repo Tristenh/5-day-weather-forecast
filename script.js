66ed45bccb6d66ab2ca5776f20ecb655f5ace1de
Tristenh <tristenhobba1@gmail.com>
2023-06-24 21:12:03 +1000

    commented HTML, CSS and Js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+// querySelectors
 const form = document.querySelector(".form");
 const input = document.querySelector(".input");
 const cityInfo = document.querySelector(".city-info");
 const dayForecast = document.querySelectorAll(".day-forecast");
 
+// current date
 let getDate = new Date();
 const currentDay = getDate.toLocaleDateString();
 
+// API key
 var APIKey = "a5a686e64400da39c5b4faa4a396c1b4";
+
+// form submit
 form.addEventListener("submit", function (event) {
   var city = input.value;
   event.preventDefault();
   getApi(city);
 });
 
+// getApi
 function getApi(city) {
   var queryURL =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -21,6 +27,7 @@ function getApi(city) {
     APIKey +
     "&units=metric";
 
+  // fetch request
   fetch(queryURL)
     .then(function (response) {
       return response.json();
@@ -29,10 +36,14 @@ function getApi(city) {
       cityInfo.innerHTML = "";
       console.log(data);
 
+      // for loop dayForcast
       for (let i = 0; i < dayForecast.length; i++) {
         let forecast = dayForecast[i];
         let futureDay = data.list[i * 8];
+
+        // if statement
         if (i >= 0) {
+          // cityInfo.innerHTML
           cityInfo.innerHTML = ` 
           <h2>${data.city.name} (${currentDay})<img  src= "https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png?"/>
           </h2>
@@ -40,13 +51,12 @@ function getApi(city) {
           <p>Wind: ${data.list[0].wind.speed} km/h</p>
           <p>Humidity: ${data.list[0].main.humidity} %</p>`;
 
+          // forecast.innerHTML
           forecast.innerHTML = `
           <h2>(${futureDay.dt_txt.split(" ")[0]})</h2>
-
            <img class="icon" src= "https://openweathermap.org/img/wn/${
              futureDay.weather[0].icon
            }@2x.png?"> 
-          
           <p>Temp: ${futureDay.main.temp} °C</p>
           <p>Wind: ${futureDay.wind.speed} km/h</p>
           <p>Humidity: ${futureDay.main.humidity} %</p>`;
