1354ce0de68159d7071b4520e5796ffa0840ce2d
Tristenh <tristenhobba1@gmail.com>
2023-06-24 11:42:19 +1000

    added cityInfo query selector, fetch.then(data) ,gets the current date, toLocaleDateString formats date in a string representation, and cityInfo uses template literals to contruct a formatted string with the (data) of the fetched api and getting the desired properties of the (data)

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const form = document.querySelector(".form");
 const input = document.querySelector(".input");
+const cityInfo = document.querySelector(".city-info");
 
 var APIKey = "a5a686e64400da39c5b4faa4a396c1b4";
 
 form.addEventListener("submit", function (event) {
   var city = input.value;
   event.preventDefault();
-  console.log("click");
   getApi(city);
 });
 
@@ -22,6 +22,14 @@ function getApi(city) {
       return response.json();
     })
     .then(function (data) {
+      let getDate = new Date();
+      cityInfo.innerHTML = ` 
+      <h2>${data.name} (${getDate.toLocaleDateString(1 / 12 / 2023)})</h2>
+     <p>Temp: ${data.main.temp}</p>
+     <p>Wind: ${data.wind.speed}km/h</p>
+     <p>Humidity: ${data.main.humidity}%</p>`;
+
+      console.log(data.main.temp);
       console.log(data);
     });
 }
